0d2f17b4d97926d2f1b17e2f95c981f4f3217451
Mara Jerkovic <mjerkovic@medallia.com>
2021-06-22 19:14:57 -0300

    cambios javascript

--- index.js
+++ index.js
@@ -10,12 +10,31 @@ mensaje = "hola esto es un mensaje"
 mensaje = "hola blabla"
 console.log(mensaje)
 
-function cambiarColor() {
-    var titulo = document.getElementById('titulo')
-    titulo.style.backgroundColor='black'
+function cambiarColor(elemento, color) {
+    var titulo = document.getElementById(elemento)
+    titulo.style.backgroundColor=color
     titulo.style.color='white'
     titulo.style.fontSize='15px'
 
-    alert("ok")
+    //alert("ok")
+    /*asfsjf
+    sdj
+    sdj
+    */
     console.log("cambio el color")
+}
+
+function mostrar(idTexto, idBoton) {
+    var texto = document.getElementById(idTexto)
+    var boton = document.getElementById(idBoton)
+    console.log("mostrar")
+    if (boton.innerHTML == "Mostrar más") {
+        console.log("if")
+        texto.style.maxHeight = "40em"
+        boton.innerHTML = "Mostrar menos"
+    } else {
+        console.log("else")
+        texto.style.maxHeight = "4em"
+        boton.innerHTML = "Mostrar más"
+    }
 }
